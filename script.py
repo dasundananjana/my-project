607381348a69248b3473ba74e14346c194313b3b
dasundananjana <dasundananjana2021@gmail.com>
2025-03-24 12:51:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,19 @@
-print("Hello, Git!")
-x=2
-y=3
-z=x+y
-print(z)
+num_students = int(input("Enter number of students: "))
+
+for i in range(num_students):
+    print(f"\nStudent {i+1}")
+    marks = int(input("Enter English marks: "))
+
+    if marks >= 75:
+        grade = "A"
+    elif marks >= 65:
+        grade = "B"
+    elif marks >= 55:
+        grade = "C"
+    elif marks >= 35:
+        grade = "S"
+    else:
+        grade = "F"
+
+    print(f"Grade for student {i+1}: {grade}")
+
